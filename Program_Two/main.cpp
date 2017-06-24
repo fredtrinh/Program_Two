4bafd04db0b2e658750e5835a39126ea7416b9b8
Fred Trinh <fred@freds-mbp.lan>
2017-06-24 13:14:53 -0500

    Initial Commit

---
+++ Program_Two/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Program_Two
+//
+//  Created by Fred Trinh on 6/24/17.
+//  Copyright © 2017 Fred Trinh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
